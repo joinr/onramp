27a471a203ace666ec2f4425d7ee187b0cf7a0e9
tom <tom@that-place>
2018-08-16 02:26:36 -0500

    Added problem descriptions

---
+++ causes.clj
@@ -0,0 +1,9 @@
+;;Taken from the popular "causes" problem that swept the internet
+;;years back...
+;; We define a “friend” of a source word as any candidate word in the
+;; dictionary that may be found altering the source word by a single
+;; character.  Alterations include substituting letters, removing
+;; letters, or adding a letters.  We can define the “social network” of a
+;; source word as any candidate word that is a friend of the source word,
+;; or a friend of the candidate word.  Given the provided set of words,
+;; what is the size of the “social network” for the word “causes” ?

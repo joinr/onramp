e4d0387dae3756022a711ba26a8594e0a80736d1
tom <tom@the-cloud>
2018-09-17 13:01:37 -0500

    Added euler problems

---
+++ eulers.clj
@@ -0,0 +1,64 @@
+﻿(ns eulers)
+
+;;Euler 102 https://projecteuler.net/problem=102
+
+;;Three distinct points are plotted at random on a Cartesian plane, 
+;;for which -1000 ≤ x, y ≤ 1000, such that a triangle is formed.
+;;Consider the following two triangles:
+;;A(-340,495), B(-153,-910), C(835,-947)
+;;X(-175,41),  Y(-421,-714), Z(574,-645)
+
+;;It can be verified that triangle ABC contains the origin, whereas triangle XYZ does not.
+;;Using triangles.txt (right click and 'Save Link/Target As...'), 
+;;a 27K text file containing the co-ordinates of one thousand "random" triangles, 
+;;find the number of triangles for which the interior contains the origin.
+
+;;NOTE: The first two examples in the file represent the triangles in the example given above.
+
+
+;;107 https://projecteuler.net/problem=107
+
+;;The following undirected network consists of seven 
+;;vertices and twelve edges with a total weight of 243.
+;;       20
+;;    B______E
+;; 16/ \     / \ 
+;;  /   \   /18 \11
+;; /  17 \ /     \ 
+;;A ______D_______\
+;; \  21 / \ 23  /G
+;;  \   /28 \   /
+;; 12\ /   19\ / 27
+;;    C ------F
+;;       31
+
+
+;;The same network can be represented by the matrix below.
+;;  A  B  C  D  E  F  G 
+;;A -  16 12 21 -  -  - 
+;;B 16 -  -  17 20 -  - 
+;;C 12 -  -  28 -  31 - 
+;;D 21 17 28 -  18 19 23 
+;;E -  20 -  18 -  -  11 
+;;F -  -  31 19 -  -  27 
+;;G -  -  -  23 11 27 - 
+
+;;However, it is possible to optimise the network by removing some edges and still 
+;;ensure that all points on the network remain connected. The network which achieves 
+;;the maximum saving is shown below. It has a weight of 93, representing a saving of
+;;243 − 93 = 150 from the original network.
+
+;;    B       E
+;; 16/ \     / \ 
+;;  /   \   /18 \11
+;; /  17 \ /     \ 
+;;A       D       \
+;; \       \       G
+;;  \       \
+;; 12\     19\
+;;    C       F
+
+
+;;Using network.txt (right click and 'Save Link/Target As...'), a 6K text file containing
+;;a network with forty vertices, and given in matrix form, find the maximum saving which 
+;;can be achieved by removing redundant edges whilst ensuring that the network remains connected.
